f2bc2cbd8978209fe3d8cb80e3cb76f9fd1435ad
Mirva Näivö <mirva62@gmail.com>
2023-12-07 15:44:47 +0200

    assigment 1.6 extrabonus

--- Lecture1/Assignment1.1/assignment1.6.js
+++ Lecture1/Assignment1.1/assignment1.6.js
@@ -6,19 +6,40 @@
 // }
 
 
-const readline = require("node:readline").createInterface({
+// const readline = require("node:readline").createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+//   });
+//   readline.question("Give statement: ", input => {
+//     output = input.trim()
+//     if(output.length <= 20) {
+//         console.log(output.toLowerCase())
+//     }
+//     else {
+//         console.log("Statement too long")
+//     }
+//     readline.close();
+//   });
+
+
+
+
+const readline2 = require("node:readline").createInterface({
     input: process.stdin,
     output: process.stdout,
   });
-  readline.question("Give statement: ", input => {
-    output = input.trim()
-    if(output.length <= 20) {
-        console.log(output.toLowerCase())
-    }
-    else {
+  readline2.question("Give statement: ", input => {
+    if(input.length > 20) {
         console.log("Statement too long")
     }
-    readline.close();
+    else if(input !== input.toLowerCase()) {
+        console.log("Statement has to be lowercase")
+    }
+    else if(input !== input.trim()) {
+        console.log("Statement can't contain empty spaces in the beginning or in the end")
+    }
+    else {
+        console.log(input)
+    }
+    readline2.close();
   });
-
-
